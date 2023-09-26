a66ade865a9a0f14f754bfb989e89de08960ef4d
vjruizumich <vjruiz@umich.edu>
2023-09-26 15:41:51 -0400

    made init method

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -5,8 +5,9 @@ class Rectangle():
     # It sets an instance variable, "width" to the passed argument, width
     # It sets an instance variable, "height" to the passed argument, height
 
-    # YOUR CODE HERE
-    #test commit
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
 
